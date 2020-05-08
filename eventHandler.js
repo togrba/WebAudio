b4ede50eb97439c362c8db237c6108c6f3fe9110
JOhohannes Loor <johannes.loor@gmail.com>
2020-05-08 14:05:30 +0200

    Made eventhandler and fixed css

---
+++ eventHandler.js
@@ -0,0 +1,23 @@
+let touchArea = document.getElementById("touchArea");
+let buttonTwo = document.getElementById("button2");
+
+touchArea.onclick = touchAreaClick;
+touchArea.onpointerdown = touchAreaDown;
+
+function touchAreaClick(event) {
+  buttonTwo.disabled = false;
+  compare(webAudioXML.lastGesture, webAudioXML.getSequence("_storedGesture"))
+    ? ((touchArea.style.cssText = "border-color:green;"),
+      (touchArea.innerHTML = "YOU DID IT! Congratulations"))
+    : (touchArea.style.cssText = "border-color:red;"),
+    (touchArea.innerHTML = "The sounds do not match.. <br/> Try again!");
+
+  event.preventDefault();
+}
+
+function touchAreaDown(event) {
+  touchArea.innerHTML = "";
+  touchArea.style.cssText = "border-color:transparent;";
+
+  event.preventDefault();
+}
